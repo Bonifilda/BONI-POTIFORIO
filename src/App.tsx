7078b03dc3406114a3fb48e66eefd308cfb48d5a
Bonifilda <bonimani2020@gmail.com>
2025-12-08 16:59:20 +0200

    Fix component import name mismatch

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Projects from "./components/Project";
 import Skills from "./components/Skills";
-import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 function App() {
   return (
